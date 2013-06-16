706976b9bc09b629fcb7bdcfd5fdc78ac8bce497
Alexander Magmai <chanaphan.tadeesom@outlook.com>
2013-06-16 12:56:54 +0700

    update list of content (frontend request)

--- sites/all/themes/omega/omega/templates/node.tpl.php
+++ sites/all/themes/omega/omega/templates/node.tpl.php
@@ -1,11 +1,8 @@
 <article<?php print $attributes; ?>>
+ <div class="blockcontroller">
   <?php print $user_picture; ?>
   <?php print render($title_prefix); ?>
-  <?php if (!$page && $title): ?>
-  <header>
-    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-  </header>
-  <?php endif; ?>
+
   <?php print render($title_suffix); ?>
   <?php if ($display_submitted): ?>
   <footer class="submitted"><?php print $date; ?> -- <?php print $name; ?></footer>
@@ -19,7 +16,14 @@
       print render($content);
     ?>
   </div>
-  
+  <?php if (!$page && $title): ?>
+  <header>
+    <p<?php print $title_attributes; ?>>
+      <!-- <a href="<?php //print $node_url ?>" title="<?php //print $title ?>"><?php //print $title ?></a> -->
+      <?php print $title ?>
+    </p>
+  </header>
+  <?php endif; ?>
   <div class="clearfix">
     <?php if (!empty($content['links'])): ?>
       <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
@@ -27,4 +31,5 @@
 
     <?php print render($content['comments']); ?>
   </div>
+</div>
 </article>
